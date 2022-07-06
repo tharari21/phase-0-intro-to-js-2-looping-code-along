86dfad75f1abc3a7f37f6d5ddfbdcf4c2023a548
Tomer Harari <tharari21@gmail.com>
2022-07-05 22:10:43 -0400

    commit tree d3988e319bb224a776c54529d63bde914124c274

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const writeCards = (names) => {
+  const messageArr = [];
+
+  for (const name of names) {
+    messageArr.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+
+  return messageArr;
+};
+const countDown = (num) => {
+  for (let i = num; i > -1; i--) {
+    console.log(i);
+  }
+};
